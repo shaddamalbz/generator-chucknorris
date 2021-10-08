cc5004dc587af07ba2fb72fd95c4983e5876e847
Shaddam Alghafiqih <shaddam.albz@gmail.com>
2021-10-08 17:50:15 +0700

    adjustment on headers, change icon, base color, font family

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -2,14 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "../assets/style.css";
+import arrowleft from "../assets/icons/arrowleft.svg";
 
 export default function Header() {
+  const pathname = window.location.pathname;
+
   return (
     <header>
-      <Link to="/" className="arrow-left" style={{ textDecoration: "none" }}>
-        &#8592;
-      </Link>
-      <div className="text-center text-primary font-bold text-lg">
+      {pathname.length > 1 && (
+        <Link to="/" className="arrow-left" style={{ textDecoration: "none" }}>
+          <img src={arrowleft} alt="icon arrow that pointing to the left" />
+        </Link>
+      )}
+      <div className="text-center text-primary font-bold header-title">
         CHUCK NORRIS
       </div>
     </header>
